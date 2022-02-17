0e6113bafa3f2a84a4ccd2df611ce3b36da0d924
Kai Cai <kacai@microsoft.com>
2022-02-18 01:21:04 +0800

    init

--- routes/appinsight.js
+++ routes/appinsight.js
@@ -1,26 +1,31 @@
 let appInsights = require('applicationinsights');
-appInsights.setup("InstrumentationKey=0ad1757f-7ce0-c570-b74f-871aa5e8b1cc;EndpointSuffix=applicationinsights.azure.cn;IngestionEndpoint=https://chinaeast2-0.in.applicationinsights.azure.cn/");
+appInsights.setup("<Application Insight Connection String>");
 appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = "appServer";
 appInsights.start();
 let client = appInsights.defaultClient;
 
+
+
 var http = require('http');
+var fs = require('fs');
 
 var express = require('express');
 const { Warning, Information } = require('applicationinsights/out/Declarations/Contracts/Generated/SeverityLevel');
 var router = express.Router();
 var metricValue = 0;
+var reg = new RegExp("\n", "g");
+var filedata = fs.readFileSync('./routes/loganalytics.kusto', 'utf8').replace(reg, "<br/>");
 
 /* GET page. */
 router.get('/', function(req, res, next) {
   metricValue	= Math.floor(Math.random()*10);
-  
+
   res.render('appinsight', { 
 	  title: 'Azure AppInsights w LogAnalytics', 
-	  metric: metricValue
-	  //timestamp: new Date().toLocaleString('zh-CN', {hour12: false}) 
+	  metric: metricValue,
+	  filedata: filedata,
+	  time: new Date().toLocaleString('zh-CN', {hour12: false}) 
 	});
-
 
   client.trackEvent({
 	  name: "KCEvent001", 
@@ -84,28 +89,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-
-// http.createServer(function (request, response) {
-//   response.writeHead(200, {'Content-Type':'text/plain'});
-//   response.end('Hello World!');  
-
-// 	client.trackEvent({name: "my custom event", properties: {customProperty: "custom property value"}});
-// 	client.trackException({exception: new Error("handled exceptions can be logged with this method")});
-// 	client.trackMetric({name: "custom metric", value: 3});
-// 	client.trackTrace({message: "trace message"});
-	
-// 	client.trackDependency({
-// 		target:"http://dbname", 
-// 		name:"select customers proc", 
-// 		data:"SELECT * FROM Customers", 
-// 		duration:231, 
-// 		resultCode:0, 
-// 		success: true, 
-// 		dependencyTypeName: "ZSQL"
-// 	});
-
-// 	client.trackRequest({name:"GET /customers", url:"http://myserver/customers", duration:309, resultCode:200, success:true});
-  
-// }).listen(3000);
-
-//console.log('Server running at http://localhost:3000/');
--- routes/test.js
+++ routes/test.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
+var filedata = "";
+
 function add(a, b) {
     return a + b;
 }
 
-var c=add(1, 2);
-console.log(c);
+//var c=add(1, 2);
+//console.log(c);
 
-console.info(new Date().toLocaleString('zh-CN', {hour12: false}));
-console.info(Math.floor(Math.random()*10));
+//console.info(new Date().toLocaleString('zh-CN', {hour12: false}));
+//console.info(Math.floor(Math.random()*10));
+
+filedata = fs.readFileSync('loganalytics.kusto', 'utf8');
+console.info(filedata);
